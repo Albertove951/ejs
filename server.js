1cf365aa90120c939a626ac6c1a6a14c9ca6b8a3
Alberto Vega <albertovega00@gmail.com>
2020-01-31 15:23:43 -0800

    added to ejs files and updated server file

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require('express'); 
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const profile = require('./profile')
 
 const app = express();
 
 app.use(morgan( 'dev' ))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use('./profile', profile)
 
 
 app.set('views', './views');
@@ -16,6 +18,14 @@ app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
     res.render('index');
+});
+
+app.get('/contact', (req, res) => {
+    res.render('contact');
+});
+
+app.post('/thanks', (req, res) => {
+    res.render('thanks', {contact: req.body})
 });
 
 app.listen(8000, () => {
