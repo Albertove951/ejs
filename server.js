b36e4cb445e39f90eb72a91c69b86a5fe05c9efd
Alberto Vega <albertovega00@gmail.com>
2020-01-31 14:45:07 -0800

    added server

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express'); 
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(morgan('dev')
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+
+app.set('views', './views');
+
+app.set('view engine', 'ejs');
+
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.listen(8000, () => {
+    console.log("server is listening on local host 8000")
+})
